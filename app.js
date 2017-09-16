12ba420ab625919d8c2ba9ba4d1b586c9f65a188
bogdanmacovei <bogdanmaco@gmail.com>
2017-09-16 23:27:25 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ var mysql = require('mysql');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var bcrypt = require('bcryptjs');
+
+function encryption (str) {
+	return bcrypt.hashSync(str, 10);
+}
+
+function verify (str, criptedstr) {
+	return bcrypt.compareSync(str, criptedstr);
+}
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static(__dirname + '/views'));
 
-var server = app.listen(process.env.PORT||8000, function(){
+var server = app.listen(process.env.PORT||8000, function() {
 	console.log('Connected to server on port ' + server.address().port);
 });
 
@@ -26,22 +35,28 @@ conn.connect(function(err){
 });
 
 conn.query(
-	'CREATE TABLE IF NOT EXISTS user1 \
+	'CREATE TABLE IF NOT EXISTS userCvorum \
 	(\
 		id int AUTO_INCREMENT PRIMARY KEY,\
 		first_name VARCHAR(20) NOT NULL,\
 		last_name VARCHAR(20) NOT NULL,\
 		user_name VARCHAR(20) NOT NULL,\
-		password VARCHAR(30) NOT NULL\
+		password VARCHAR(100) NOT NULL,\
+		joined VARCHAR(20),\
+		birth_date VARCHAR(20),\
+		country VARCHAR(20),\
+		short_description VARCHAR(300),\
+		img VARCHAR(300)\
 	)',
 	function(err, res){
 		if(err)
 			throw err;
-		console.log(res);
+		//console.log(res);
 });
 
+
 conn.query(
-	'CREATE TABLE IF NOT EXISTS post\
+	'CREATE TABLE IF NOT EXISTS postCvorum\
 	(\
 		id int AUTO_INCREMENT PRIMARY KEY,\
 		title VARCHAR(50) NOT NULL,\
@@ -51,41 +66,22 @@ conn.query(
 	)',
 	function(err, res){
 		if(err) throw err;
-		console.log(res);
+		//console.log(res);
 });
 
 conn.query(
-	'CREATE TABLE IF NOT EXISTS comment\
+	'CREATE TABLE IF NOT EXISTS commentCvorum\
 	(\
 		id int AUTO_INCREMENT PRIMARY KEY,\
 		comment VARCHAR(300),\
 		date VARCHAR(20),\
 		user_id int REFERENCES user(id),\
-		post_id int REFERENCES post(id)\
+		post_id int REFERENCES postCvorum(id)\
 	)',
 	function(err, res){
 		if(err) throw err;
-		console.log(res);
+		//console.log(res);
 });
-
-
-// conn.query(
-// 	'ALTER TABLE user1\
-// 	ADD joined VARCHAR(20),\
-// 	ADD birth_date VARCHAR(20),\
-// 	ADD country VARCHAR(20),\
-// 	ADD short_description VARCHAR(300),\
-// 	ADD img VARCHAR(100)',
-// 	function(err, res){
-// 		if(err) throw err;
-// 		console.log(res);
-// });
-
-conn.query('SELECT * FROM user1', function(err, res) {
-	if(err) throw err;
-	console.log(res);
-});
-
 
 //get pages
 app.get('/register', function(req, res){
@@ -115,14 +111,14 @@ app.get('/profile', function(req, res){
 
 //get user
 app.get('/selectuserwhere/:name', function(req, res) {
-	conn.query('SELECT id, password FROM user1 WHERE user_name = ?', req.params.name, function(err, result){
+	conn.query('SELECT id FROM userCvorum WHERE user_name = ?', req.params.name, function(err, result){
 		if(err) throw err;
 		res.send(result);
 	});
 });
 
 app.get('/selectuserbyid/:id', function(req, res) {
-	conn.query('SELECT first_name, img FROM user1 WHERE id = ?', req.params.id, function(err, result){
+	conn.query('SELECT first_name, img FROM userCvorum WHERE id = ?', req.params.id, function(err, result){
 		if(err) throw err;
 		res.send(result);
 	});
@@ -130,7 +126,7 @@ app.get('/selectuserbyid/:id', function(req, res) {
 
 app.get('/selectalluserbyid/:id', function(req, res) {
 	conn.query('SELECT first_name, last_name, user_name, joined, img, country, short_description, birth_date\
-	 			FROM user1 WHERE id = ?', req.params.id, function(err, result){
+	 			FROM userCvorum WHERE id = ?', req.params.id, function(err, result){
 		if(err) throw err;
 		res.send(result);
 	});
@@ -139,18 +135,19 @@ app.get('/selectalluserbyid/:id', function(req, res) {
 // select * from post
 
 app.get('/selectpost', function(req, res){
-	conn.query('SELECT * FROM post', function(err, result) {
+	conn.query('SELECT * FROM postCvorum', function(err, result) {
 		if (err) throw err;
 		res.send(result);
 	});
 });
 
 //get post / user
-app.get('/selectallpost', function(req, res){
+
+app.get('/selectallpost/:pid', function(req, res) {
 	conn.query(
 		'SELECT b.title, b.content, b.date, a.user_name, b.id, b.user_id\
-		FROM user1 a, post b\
-		WHERE a.id = b.user_id',
+		FROM userCvorum a, postCvorum b\
+		WHERE a.id = b.user_id ORDER BY b.date DESC LIMIT ?, 5', (req.params.pid-1) * 5,
 		function(err, result){
 			if(err) throw err;
 			res.send(result);
@@ -158,24 +155,27 @@ app.get('/selectallpost', function(req, res){
 });
 
 //get delete post
-app.get('/deletepost/:id', function(req, res){
-	conn.query('DELETE FROM post WHERE id = ?', req.params.id, function(err, result){
+
+app.get('/deletepost/:id', function(req, res) {
+	conn.query('DELETE FROM postCvorum WHERE id = ?', req.params.id, function(err, result){
 		if(err) throw err;
 		res.send(result);
 	});
 });
 
 //get detele comment
-app.get('/deletecomment/:id', function(req, res){
-	conn.query('DELETE FROM comment WHERE id = ?', req.params.id, function(err, result){
+
+app.get('/deletecomment/:id', function(req, res) {
+	conn.query('DELETE FROM commentCvorum WHERE id = ?', req.params.id, function(err, result){
 		if(err) throw err;
 		res.send(result);
 	});
 });
 
 //get detele comment from a specific post
-app.get('/deletecommentpost/:id', function(req, res){
-	conn.query('DELETE FROM comment WHERE post_id = ?', req.params.id, function(err, result){
+
+app.get('/deletecommentpost/:id', function(req, res) {
+	conn.query('DELETE FROM commentCvorum WHERE post_id = ?', req.params.id, function(err, result){
 		if(err) throw err;
 		res.send(result);
 	});
@@ -183,8 +183,9 @@ app.get('/deletecommentpost/:id', function(req, res){
 
 
 //get post by post id
+
 app.get('/post/:id', function(req, res){
-	conn.query('SELECT a.*, b.* FROM post b, user1 a WHERE a.id = b.user_id AND b.id = ?', req.params.id, function(err, result){
+	conn.query('SELECT a.id, a.first_name, a.last_name, a.user_name, b.* FROM postCvorum b, userCvorum a WHERE a.id = b.user_id AND b.id = ?', req.params.id, function(err, result){
 		if(err) throw err;
 		res.send(result);
 	});
@@ -192,9 +193,9 @@ app.get('/post/:id', function(req, res){
 
 //get comments by page id
 
-app.get('/selectallcomments/:pid', function(req, res){
+app.get('/selectallcomments/:pid', function(req, res) {
 	conn.query('SELECT c.comment, c.date, a.user_name, a.img, c.user_id, c.id\
-				FROM user1 a, post b, comment c\
+				FROM userCvorum a, postCvorum b, commentCvorum c\
 				WHERE c.post_id = b.id AND c.user_id = a.id AND c.post_id = ?', req.params.pid, function(err, result){
 					if(err) throw err;
 					res.send(result);
@@ -205,7 +206,7 @@ app.get('/selectallcomments/:pid', function(req, res){
 
 app.get('/selectallcommentsuser/:uid', function(req, res) {
 	conn.query('SELECT c.comment, c.date, b.title, b.id\
-				FROM user1 a, post b, comment c\
+				FROM userCvorum a, postCvorum b, commentCvorum c\
 				WHERE c.user_id = a.id AND c.post_id = b.id AND a.id = ? ORDER BY c.date DESC', req.params.uid, function(err, result) {
 					if (err) throw err;
 					res.send(result);
@@ -216,19 +217,30 @@ app.get('/selectallcommentsuser/:uid', function(req, res) {
 
 app.get('/selectallpostsuser/:uid', function(req, res) {
 	conn.query('SELECT b.title, b.id, b.date\
-				FROM user1 a, post b\
+				FROM userCvorum a, postCvorum b\
 				WHERE a.id = b.user_id AND a.id = ? ORDER BY b.date DESC', req.params.uid, function(err, result) {
 					if (err) throw err;
 					res.send(result);
 				});
 });
 
+// post nr
+
+app.get('/postnumber', function(req, res) {
+	conn.query('SELECT COUNT(id) AS number FROM postCvorum', function(err, result) {
+		if (err)
+			throw err;
+		res.send (result);
+	});
+});
+
 
 //post user
-app.post('/insertuser', function(req, res){
-	var arr = [[req.body.fn, req.body.ln, req.body.un, req.body.pw, req.body.jd]];
+
+app.post('/insertuser', function(req, res) {
+	var arr = [[req.body.fn, req.body.ln, req.body.un, encryption(req.body.pw), req.body.jd]];
 	
-	conn.query('INSERT INTO user1 (first_name, last_name, user_name, password, joined) VALUES ?', [arr], function(err, result){
+	conn.query('INSERT INTO userCvorum (first_name, last_name, user_name, password, joined) VALUES ?', [arr], function(err, result){
 		if(err) 
 			throw err;
 	});
@@ -237,12 +249,13 @@ app.post('/insertuser', function(req, res){
 
 
 //post content
-app.post('/insertpost', function(req, res){
+
+app.post('/insertpost', function(req, res) {
 	var txt = '\"' + req.body.title + '\", \"' + req.body.content + '\", \"' + req.body.user_id + '\", \"'
 	 + req.body._date + '\"';
-	var sql = 'INSERT INTO post(title, content, user_id, date) VALUES(' + txt + ')';
-	console.log(sql);
-	conn.query(sql, function(err, result){
+	var sql = 'INSERT INTO postCvorum (title, content, user_id, date) VALUES(' + txt + ')';
+	//console.log(sql);
+	conn.query(sql, function(err, result) {
 		if(err) throw err;
 	});
 	res.end();
@@ -250,23 +263,23 @@ app.post('/insertpost', function(req, res){
 
 //post comment 
 
-app.post('/insertcomment', function (req, res){
+app.post('/insertcomment', function (req, res) {
 	var txt = '\"' + req.body.comment + '\", \"' + req.body._date + '\", \"' + req.body.user_id + '\", \"' + req.body.post_id + '\"';
-	var sql = 'INSERT INTO comment (comment, date, user_id, post_id) VALUES (' + txt + ')';
+	var sql = 'INSERT INTO commentCvorum (comment, date, user_id, post_id) VALUES (' + txt + ')';
 
-	conn.query(sql, function(err, result){
+	conn.query(sql, function(err, result) {
 		if(err) throw err;
 	});
 	res.end();
 });
 
 app.post('/updateuser', function(req, res) {
-	var sql = 'UPDATE user1 SET img = ' + '"' + req.body.img + '", ' + 
+	var sql = 'UPDATE userCvorum SET img = ' + '"' + req.body.img + '", ' + 
 				 'short_description = ' + '"' + req.body.description + '", ' + 
 				           'country = ' + '"' + req.body.country + '", ' + 
 				        'birth_date = ' + '"' + req.body.birth_date + '" WHERE id = ' + '"' + req.body.id + '"';
 
-	console.log(sql);
+	//console.log(sql);
 
 	conn.query(sql, function(err, result) {
 		if (err) throw err;
@@ -274,4 +287,19 @@ app.post('/updateuser', function(req, res) {
 
 	res.end();
 
+});
+
+// verific parola
+
+app.post('/verifypwd', function (req, res) {
+	conn.query('SELECT password FROM userCvorum WHERE user_name = ?', req.body.username, function (err, result) {
+		if (err) throw err;
+
+		var isValid = false;
+
+		if (verify (req.body.password, result[0].password))
+			isValid = true;
+
+		res.send (isValid);
+	});
 });
